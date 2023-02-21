5cb87a0328bdbc5d3692d2a74748c36744eb8b50
Maksim Botalov <dev.botalov@gmail.com>
2023-02-21 21:39:30 +0500

    Initial Commit

---
+++ UnderControl/UnderControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderControlApp.swift
+//  UnderControl
+//
+//  Created by Максим Боталов on 21.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct UnderControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
